065a91b81c27535027a070923d1dd42b362bb3df
Wesley Borges <djwesleyborges@gmail.com>
2023-07-05 15:58:23 -0300

    ref: reparado render_to_string

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -37,5 +37,5 @@ def new(request):
 
 
 def _send_mail(subject, from_, to, template_name, context):
-    body = render_to_string(template_name, context, subject)
+    body = render_to_string(template_name, context)
     mail.send_mail(subject, body, from_, [from_, to])
